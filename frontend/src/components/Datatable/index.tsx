4666647d1c76395f45155a3ac4d330fb9cd5173e
JorgeSicsu <jlsicsu@hotmail.com>
2021-11-08 01:09:19 -0300

    Basic static components

---
+++ frontend/src/components/Datatable/index.tsx
@@ -0,0 +1,112 @@
+const Datatable = () => {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>25/08/2021</td>
+                        <td>Jorge Bastos</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>9800.00</td>
+                    </tr>
+                    <tr>
+                        <td>26/09/2021</td>
+                        <td>Beatriz Domingues</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>1547.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Deise Peixoto</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>2390.80</td>
+                    </tr>
+                    <tr>
+                        <td>25/08/2021</td>
+                        <td>Jorge Bastos</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>9800.00</td>
+                    </tr>
+                    <tr>
+                        <td>25/08/2021</td>
+                        <td>Jorge Bastos</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>9800.00</td>
+                    </tr>
+                    <tr>
+                        <td>25/08/2021</td>
+                        <td>Jorge Bastos</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>9800.00</td>
+                    </tr>
+                    <tr>
+                        <td>25/08/2021</td>
+                        <td>Jorge Bastos</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>9800.00</td>
+                    </tr>
+                    <tr>
+                        <td>25/08/2021</td>
+                        <td>Jorge Bastos</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>9800.00</td>
+                    </tr>
+                    <tr>
+                        <td>25/08/2021</td>
+                        <td>Jorge Bastos</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>9800.00</td>
+                    </tr>
+                    <tr>
+                        <td>25/08/2021</td>
+                        <td>Jorge Bastos</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>9800.00</td>
+                    </tr>
+                    <tr>
+                        <td>25/08/2021</td>
+                        <td>Jorge Bastos</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>9800.00</td>
+                    </tr>
+                    <tr>
+                        <td>25/08/2021</td>
+                        <td>Jorge Bastos</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>9800.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Datatable;
